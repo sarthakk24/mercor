889ef8c43b8a03ab29ba9fab42ec48c551f11ed0
Sarthak Sachdeva <sarthak.sachdeva.73@gmail.com>
2023-07-01 17:44:42 +0530

    add flowbite footer

---
+++ client/src/components/home/inputModule.tsx
@@ -0,0 +1,10 @@
+const InputModule = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Description" />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default InputModule;
---
+++ client/src/components/shared/footer.tsx
@@ -0,0 +1,109 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-gray-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="https://flowbite.com/" className="flex items-center">
+              <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                Content Generation
+              </span>
+            </a>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:grid-cols-4 sm:gap-6">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Project
+              </h2>
+              <ul className="font-medium text-gray-600 dark:text-gray-400">
+                <li className="mb-4">
+                  <a href="https://flowbite.com/" className="hover:underline">
+                    Github
+                  </a>
+                </li>
+                <li className="mb-4">
+                  <a
+                    href="https://tailwindcss.com/"
+                    className="hover:underline"
+                  >
+                    Youtube Video
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindcss.com/"
+                    className="hover:underline"
+                  >
+                    Presentation
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Sarthak Sachdeva
+              </h2>
+              <ul className="font-medium text-gray-600 dark:text-gray-400">
+                <li className="mb-4">
+                  <a href="https://flowbite.com/" className="hover:underline">
+                    Flowbite
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindcss.com/"
+                    className="hover:underline"
+                  >
+                    Tailwind CSS
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Saransh Sinha
+              </h2>
+              <ul className="font-medium text-gray-600 dark:text-gray-400">
+                <li className="mb-4">
+                  <a
+                    href="https://github.com/themesberg/flowbite"
+                    className="hover:underline "
+                  >
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://discord.gg/4eeurUVvTy"
+                    className="hover:underline"
+                  >
+                    Discord
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Sorabh Singh
+              </h2>
+              <ul className="font-medium text-gray-600 dark:text-gray-400">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
