4b6e20ffa1303db0ac3cfa1aa22c8e31d479a2de
Sarthak Sachdeva <sarthak.sachdeva.73@gmail.com>
2023-07-02 22:55:26 +0530

    Revert "fix vercel build issue"
    This reverts commit 6cb28de0f68c80ff004d0ffcd3ec458f691be37a.

--- client/src/components/shared/navbar.tsx
+++ client/src/components/shared/navbar.tsx
@@ -1,14 +1,12 @@
-import Link from "next/link";
-
 const Navbar = () => {
   return (
     <nav className="border-gray-200 bg-gray-50 dark:border-gray-700 dark:bg-gray-800">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <Link href="/" className="flex items-center">
+        <a href="/" className="flex items-center">
           <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
             Post IT.
           </span>
-        </Link>
+        </a>
       </div>
     </nav>
   );
